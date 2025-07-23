ecbb7868834e939f2dd8411aeaf0e00b852de451
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-07-23 15:12:19 +0900

    feat: 네비게이션바 추가

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,11 +1,9 @@
 <template>
   <nav class="navbar">
-    <!-- 왼쪽: 로고 -->
+    <!-- 내용만 있음, Navbar 자체를 또 호출하지 않음 -->
     <div class="nav-left">
       <RouterLink to="/" class="logo">LOGO</RouterLink>
     </div>
-
-    <!-- 가운데: 메뉴 -->
     <div class="nav-center">
       <RouterLink to="/ai-preconsult">AI사전상담</RouterLink>
       <RouterLink to="/cases">판례 검색</RouterLink>
@@ -13,12 +11,10 @@
       <RouterLink to="/ai-application">AI상담신청서</RouterLink>
       <RouterLink to="/video">화상상담</RouterLink>
     </div>
-
-    <!-- 오른쪽: 마이페이지 / 로그인 / 로그아웃 -->
     <div class="nav-right">
       <RouterLink to="/mypage">마이페이지</RouterLink>
-      <button @click="logout" v-if="authStore.isLoggedIn">Logout</button>
-      <RouterLink to="/login" v-else>Login</RouterLink>
+      <button @click="logout" v-if="authStore.isLoggedIn">로그아웃</button>
+      <RouterLink to="/login" v-else>로그인</RouterLink>
     </div>
   </nav>
 </template>
--- frontend/src/features/auth/user/SocialLogin.vue
+++ frontend/src/features/auth/user/SocialLogin.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
-    <h2>간편 로그인</h2>
-    <button @click="login('kakao')">카카오 로그인</button>
-    <button @click="login('naver')">네이버 로그인</button>
+  <div class="login-wrapper">
+    <h1>로그인</h1>
+    <div class="social-login-box">
+      <p>간편 로그인</p>
+      <button @click="login('kakao')">카카오 로그인</button>
+      <button @click="login('naver')">네이버 로그인</button>
+      <div class="divider">변호사이신가요?</div>
+      <button>변호사로 로그인</button>
+    </div>
+
+    <div class="footer-links">
+      <router-link to="/reset-password">비밀번호 찾기</router-link>
+      <router-link :to="{ name: 'SignUpFirst' }">변호사 회원가입하기</router-link>
+    </div>
   </div>
 </template>
 
