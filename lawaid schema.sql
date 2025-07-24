b94d03c27a53f4a784a2167e4b9ca27ee7b3bfc7
전해지 <wjsgowl0224@naver.com>
2025-07-24 15:53:25 +0900

    feat: lawyer 테이블 registration_number 컬럼 추가

--- lawaid schema.sql
+++ lawaid schema.sql
@@ -15,6 +15,7 @@ create table lawyer (
     login_password_hash varchar(100) not null, -- 비밀번호가 30byte 제한이라면, 단방향 암호화하고 Base64로 저장했을때 100byte를 절대 넘지 않을 것
     name varchar(30) not null,
     introduction text,
+    registration_number varchar(50),
     certification_status enum('PENDING', 'APPROVED', 'REJECTED') not null default 'PENDING',
     consultation_count int unsigned not null default 0
 );
