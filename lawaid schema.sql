bd5a208aad14804884f0fd0a74f7ca6c458c24f7
전해지 <wjsgowl0224@naver.com>
2025-07-24 13:55:36 +0900

    feat: lawyer 테이블 name 컬럼 추가

--- lawaid schema.sql
+++ lawaid schema.sql
@@ -13,8 +13,9 @@ create table lawyer (
 	id int unsigned auto_increment primary key,
 	login_email varchar(100) unique,
     login_password_hash varchar(100) not null, -- 비밀번호가 30byte 제한이라면, 단방향 암호화하고 Base64로 저장했을때 100byte를 절대 넘지 않을 것
+    name varchar(30) not null,
     introduction text,
-    cirtification_status enum('PENDING', 'APPROVED', 'REJECTED') not null default 'PENDING',
+    certification_status enum('PENDING', 'APPROVED', 'REJECTED') not null default 'PENDING',
     consultation_count int unsigned not null default 0
 );
 
