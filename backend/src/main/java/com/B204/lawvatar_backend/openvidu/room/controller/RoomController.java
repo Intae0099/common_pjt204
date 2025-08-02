14bbe48cbc1cf0ed8cc4aef4baa36eada71d4583
전해지 <wjsgowl0224@naver.com>
2025-08-02 20:33:05 +0900

    feat: 테스트 통과

--- backend/src/main/java/com/B204/lawvatar_backend/openvidu/room/controller/RoomController.java
+++ backend/src/main/java/com/B204/lawvatar_backend/openvidu/room/controller/RoomController.java
@@ -20,6 +20,12 @@ public class RoomController {
     private final RoomService roomService;
 
     // Method
+    /**
+     * 유저가 상담에 대한 화상상담방을 생성하기 요청을 할 때 호출되는 메서드
+     * @param authentication
+     * @param appointmentId
+     * @return
+     */
     @PostMapping("/{appointmentId}")
     public ResponseEntity<CreateRoomResponse> createRoom(Authentication authentication, @PathVariable Long appointmentId) throws Exception {
 
@@ -39,6 +45,12 @@ public class RoomController {
         return ResponseEntity.status(HttpStatus.OK).body(createRoomResponse);
     }
 
+    /**
+     * 유저가 화상상담방에 참여하기 요청을 할 때 호출되는 메서드
+     * @param authentication
+     * @param appointmentId
+     * @return
+     */
     @PostMapping("/{appointmentId}/participants") // 특정 방에 참가자를 추가로 생성(Post요청)한다는 의미에서 요청경로 이렇게 작성
     public ResponseEntity<ParticipateRoomResponse> participateRoome(Authentication authentication, @PathVariable Long appointmentId) throws Exception {
 
@@ -56,5 +68,27 @@ public class RoomController {
         ParticipateRoomResponse participateRoomResponse = ParticipateRoomResponse.builder().token(token).build();
 
         return ResponseEntity.status(HttpStatus.OK).body(participateRoomResponse);
+    }
+
+    /**
+     * 유저가 화상상담방 나가기 요청을 할 때 호출되는 메서드
+     * @param authentication
+     * @param appointmentId
+     * @return
+     */
+    @DeleteMapping("/{appointmentId}/participants/me")
+    public ResponseEntity<Void> leaveRoom(Authentication authentication, @PathVariable Long appointmentId) {
+
+        // Principal 객체 얻기
+        Object principal = authentication.getPrincipal();
+
+        // 유저타입을 서비스에 넘겨주면서 비즈니스 로직 시작
+        if(principal instanceof ClientPrincipal clientPrincipal) {
+            roomService.leaveRoom(appointmentId, "CLIENT", clientPrincipal.getId());
+        } else if(principal instanceof LawyerPrincipal lawyerPrincipal) {
+            roomService.leaveRoom(appointmentId, "LAWYER", lawyerPrincipal.getId());
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
