80b70785ae3280f383963003f2f729a02b8a92bd
전해지 <wjsgowl0224@naver.com>
2025-08-07 14:06:10 +0900

    feat: 요청 dto 검증로직 추가

--- backend/src/main/java/com/B204/lawvatar_backend/application/dto/AddApplicationRequest.java
+++ backend/src/main/java/com/B204/lawvatar_backend/application/dto/AddApplicationRequest.java
@@ -1,5 +1,6 @@
 package com.B204.lawvatar_backend.application.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.util.HashMap;
@@ -13,9 +14,15 @@ import java.util.Map;
 public class AddApplicationRequest {
 
     // Field
+    @NotBlank(message = "[AddApplicationRequest - 001] 제목은 필수입력 값입니다.")
     private String title;
+
+    @NotBlank(message = "[AddApplicationRequest - 002] 요약은 필수입력 값입니다.")
     private String summary;
+
+    @NotBlank(message = "[AddApplicationRequest - 003] 본문은 필수입력 값입니다.")
     private String content;
+
     private String outcome;
     private String disadvantage;
     private Map<String, String> recommendedQuestion = new HashMap<>();
